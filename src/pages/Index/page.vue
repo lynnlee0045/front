ca5e639848f1d890cfad1ff2c0998bdff36febe0
Kuan Lu <czlukuan@gmail.com>
2018-08-28 15:09:53 +0800

    更改首页样式

--- src/pages/Index/page.vue
+++ src/pages/Index/page.vue
@@ -1,8 +1,9 @@
 <template>
   <d2-container >
-    <div id='mapChart' style="height: 400px; width: 80%;display:inline-block" >
+    <div id="wrapper">
+      <div id='mapChart' style="height: 400px; width: 80%;display:inline-block" ></div>
+      <div id='static' style=" background-color: black; height: 500px; width: 20%;display:inline-block" >
     </div>
-    <div id='static' style=" background-color: black; height: 400px; width: 20%;display:inline-block" >
     </div>
   </d2-container>
 </template>
@@ -27,14 +28,31 @@ export default {
         myChart.setOption({
             title: { text: '在Vue中使用echarts' },
             tooltip: {},
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            // xAxis: {
+            //     data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            // },
+            // yAxis: {},
+            itemStyle: {
+              // 阴影的大小
+              shadowBlur: 100,
+              // 阴影水平方向上的偏移
+              shadowOffsetX: 0,
+              // 阴影垂直方向上的偏移
+              shadowOffsetY: 0,
+              // 阴影颜色
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
             },
-            yAxis: {},
             series: [{
                 name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
+                type: 'pie',
+                radius: '55%',
+                data: [
+                  {value:235, name:'视频广告'},
+                  {value:274, name:'联盟广告'},
+                  {value:310, name:'邮件营销'},
+                  {value:335, name:'直接访问'},
+                  {value:400, name:'搜索引擎'}
+                ]
             }]
         });
     }
@@ -52,6 +70,9 @@ export default {
     &:hover {
       color: $color-text-main;
     }
-  }
+  };
+}
+.wrapper {
+  height: 500px;
 }
 </style>
